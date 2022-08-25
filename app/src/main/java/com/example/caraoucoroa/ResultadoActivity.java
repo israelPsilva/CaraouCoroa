794ee2649f23399a78d701f5afc78c6e165de17c
israel.silva <israelpossidonio9@gmail.com>
2022-08-25 19:43:31 -0300

    Final commit

---
+++ app/src/main/java/com/example/caraoucoroa/ResultadoActivity.java
@@ -0,0 +1,45 @@
+package com.example.caraoucoroa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class ResultadoActivity extends AppCompatActivity {
+
+    private Button btnVoltar;
+    private ImageView imgResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_resultado );
+
+         btnVoltar   =  findViewById( R.id. btnVoltar);
+        imgResultado =  findViewById( R.id.imgResultado );
+
+        //recuperando dados
+        Bundle dados = getIntent().getExtras();
+        int num = dados.getInt( "numero" );
+
+        if(num == 0){
+            //cara
+            imgResultado.setImageResource( R.drawable.moeda_cara );
+        }else{
+            //coroa
+            imgResultado.setImageResource( R.drawable.moeda_coroa);
+        }
+
+        btnVoltar.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity( intent );
+            }
+        } );
+
+    }
+}
